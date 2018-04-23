aa2502de59d4eeb1869f9c16ce3b8b37c59d12cc
Dawid Śliwiński <sliwinskidawid12@gmail.com>
2018-04-23 11:22:55 +0200

    Render Canvas

--- js/app.js
+++ js/app.js
@@ -5,15 +5,19 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    
+    
+    
+    
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = {
+    update: function() {},
+    render: function() {
+    },
+    handleInput: function() {}
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = new Array();
+allEnemies[0] = new Enemy();
+allEnemies[1] = new Enemy();
+allEnemies[2] = new Enemy();
 
 
+Enemy.prototype.render();
+
+var player = player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
